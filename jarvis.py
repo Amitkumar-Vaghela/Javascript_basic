d7151c4134de1096af062033a39baaac24c4b29d
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-01-05 22:00:00 +0530

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,43 @@
+import pyttsx3
+import speech_recognition as sr
+
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 150)
+
+def speak(audio):
+  engine.say(audio)
+  engine.runAndWait()
+
+def takeCommand():
+  
+  r = sr.Recognizer()
+  with sr.Microphone() as source:
+    print("Listening...")
+    r.pause_threshold = 1
+    audio = r.listen(source)
+  try:
+    print("Recognizing...")
+    query = r.recognize_google(audio, language='en-in')
+    print(f"User said: {query}\n")
+
+  except Exception as e:
+  
+    print("Say that again please...")
+    speak("Say that again please...")
+    return "None"
+  return query
+
+if __name__ == "__main__":
+  while True:
+    query = takeCommand().lower()
+    if 'jarvis' in query:
+      print("yes sir")
+      speak("yes sir")
+
+
+
+
