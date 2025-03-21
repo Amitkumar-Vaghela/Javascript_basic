7adb9691d8c9f4394b8884ee09cd8eb10c9afa2b
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-03-21 20:02:25 +0530

    Update 03_arrowFunction.js

--- basic_03/03_arrowFunction.js
+++ basic_03/03_arrowFunction.js
@@ -12,3 +12,11 @@ console.log(this); // In a browser: Window object, In Node.js: empty object {}
 
 
 
+// Use Arrow function in For loop 
+const numbers = [10, 20, 30, 40, 50];
+let sum = 0;
+numbers.forEach(num => sum += num);
+console.log("Sum:", sum); // Output: Sum: 150
+
+
+
