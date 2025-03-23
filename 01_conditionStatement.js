3cb626f16468e270a714dd689b7239cb27d759b5
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-03-23 17:12:44 +0530

    Add files via upload

---
+++ 01_conditionStatement.js
@@ -0,0 +1,39 @@
+// if condition statement
+let age = 20;
+if (age >= 18) {
+    console.log("You are an adult.");
+}
+
+// if else condition  statement
+let num = 10;
+if (num % 2 === 0) {
+    console.log("Even number");
+} else {
+    console.log("Odd number");
+}
+
+// else-if Ladder statement
+let marks = 75;
+if (marks >= 90) {
+    console.log("Grade: A");
+} else if (marks >= 70) {
+    console.log("Grade: B");
+} else {
+    console.log("Grade: C");
+}
+
+
+// switch Statement
+let day = "Friday";
+switch (day) {
+    case "Monday":
+        console.log("Start of the week!");
+        break;
+    case "Friday":
+        console.log("Weekend is near!");
+        break;
+    default:
+        console.log("It's a regular day.");
+}
+
+
