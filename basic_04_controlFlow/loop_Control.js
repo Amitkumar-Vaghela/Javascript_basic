1cf4841c320ec3858fe9ba2f258ed7a2f539f416
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-04-29 22:18:55 +0530

    Create loop_Control.js

---
+++ basic_04_controlFlow/loop_Control.js
@@ -0,0 +1,12 @@
+// breck
+for (let i = 0; i < 10; i++) {
+  if (i === 5) break;
+  console.log(i);
+}
+
+
+// continue
+for (let i = 0; i < 5; i++) {
+  if (i === 2) continue;
+  console.log(i);
+}
