aabed837d1e795f8a928b5febebadd258cea1b39
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-06-13 21:42:28 +0530

    Add files via upload

---
+++ getElemtbyID.js
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Simple HTML Page</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is a simple HTML page.</p>
+    <div>
+        <p id = "Message">This DOM BASIC_1 file.</p>
+    </div>
+
+    <script>
+        let msg = document.getElementById("Message");
+        console.log(msg);
+    </script>
+</body>
+</html>
